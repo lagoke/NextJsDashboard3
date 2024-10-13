37648675d93b51d17a1f942d12a4a4ab36aface3
lagoke <72529997+lagoke@users.noreply.github.com>
2024-10-13 17:14:25 -0400

    New Fixes

--- auth.config.ts
+++ auth.config.ts
@@ -1,19 +1,23 @@
-import type { NextAuthConfig } from 'next-auth';
- 
+import type { NextAuthConfig, Session } from 'next-auth';
+import type { NextRequest } from 'next/server';
+
 export const authConfig = {
   pages: {
     signIn: '/login',
   },
   callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
+    authorized({ auth, request }: { auth: Session | null; request: NextRequest }) {
       const isLoggedIn = !!auth?.user;
+      const nextUrl = new URL(request.url);
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+        return Response.redirect(new URL('/dashboard', nextUrl.toString()));
       }
+
       return true;
     },
   },
